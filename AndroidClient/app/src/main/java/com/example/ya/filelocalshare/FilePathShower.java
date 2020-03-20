5a28e81127c9f556c3bac5232d23a510bca7ca55
AngryRectangle <yashmel2000@gmail.com>
2020-03-20 23:15:16 +0400

    FilePathShower refactoring

--- AndroidClient/app/src/main/java/com/example/ya/filelocalshare/FilePathShower.java
+++ AndroidClient/app/src/main/java/com/example/ya/filelocalshare/FilePathShower.java
@@ -1,10 +1,7 @@
 package com.example.ya.filelocalshare;
 
 import android.app.Activity;
-import android.content.Context;
 import android.content.res.Resources;
-import android.text.Layout;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.HorizontalScrollView;
@@ -12,75 +9,83 @@ import android.widget.LinearLayout;
 import android.widget.ScrollView;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
-import java.io.File;
-
 public class FilePathShower {
-    int pathElementView;
-    Resources res;
-    LinearLayout parent;
-    Activity activity;
     public SelectPathHandler handler;
-    public FilePathShower(Activity activity, int pathElement, LinearLayout parent){
+    private int pathElementView;
+    private Resources res;
+    private LinearLayout parent;
+    private Activity activity;
+
+    public static abstract class SelectPathHandler {
+        public abstract void execute(String fullpath);
+    }
+
+    public FilePathShower(Activity activity, int pathElement, LinearLayout parent) {
         res = activity.getApplicationContext().getResources();
         pathElementView = pathElement;
         this.parent = parent;
         this.activity = activity;
     }
-    public void showPath(String path, FileExplorer explorer){
-        parent.removeAllViews();
-        String[] parts = new Path(path).getPathElements();
-        String fullPath = "/"+parts[0]+"/";
-        parts[0] = res.getString(R.string.default_path_name);
-        addElement(parts[0]+"/", fullPath, explorer, true);
-        for(int i =1; i<parts.length; i++){
-            fullPath+=parts[i]+"/";
-            addElement(parts[i]+"/", fullPath, explorer, true);
-        }
-        final HorizontalScrollView scrollview =  (activity.findViewById(R.id.pathScroller));
-        scrollview.post(new Runnable() {
-            @Override
-            public void run() {
-                scrollview.fullScroll(ScrollView.FOCUS_RIGHT);
-            }
-        });
+
+    public void showPath(String path, FileExplorer explorer) {
+        clear();
+        addElements(path, explorer);
+        scrollToEnd((HorizontalScrollView) activity.findViewById(R.id.pathScroller));
     }
-    public void addElement(String name, @Nullable String fullPath, @Nullable FileExplorer explorer, boolean isClickable){
-        View view = inflateView(activity).findViewById(R.id.pathTextView);
-        ((TextView)view).setText(name);
-        if(isClickable){
+
+    public void addElement(
+            @NonNull String name,
+            @Nullable String fullPath,
+            @Nullable FileExplorer explorer,
+            boolean isClickable
+    ) {
+        TextView view = inflateView(activity).findViewById(R.id.pathTextView);
+        view.setText(name);
+        if (isClickable)
             setPathOnClickAction(view, fullPath);
-        }
         parent.addView((View) view.getParent());
     }
-    private String[] getPathParts(String path){
-        path = path.replaceAll(res.getString(R.string.default_path),
-                res.getString(R.string.default_path_name)+"/");
-        String[] out = path.split("[/]");
-        out[0] = res.getString(R.string.default_path).split("[/]")[0];
-        return out;
+
+    private void addElements(String path, FileExplorer explorer) {
+        String[] parts = new Path(path).getPathElements();
+        StringBuilder fullPath = new StringBuilder("/");
+        fullPath.insert(1, parts[0]);
+        parts[0] = res.getString(R.string.default_path_name);
+        addElement(parts[0], fullPath.toString(), explorer, true);
+        for (int i = 1; i < parts.length; i++) {
+            fullPath.append(parts[i]);
+            addElement(parts[i], fullPath.toString(), explorer, true);
+        }
     }
-    private void applyPathElement(String fullPath, String name, View view, final FileExplorer explorer){
-        ((TextView)view).setText(name);
-        setPathOnClickAction(view, fullPath);
-    }
-    private View inflateView(Activity activity){
+
+    private View inflateView(@NonNull Activity activity) {
         LayoutInflater ltInflater = activity.getLayoutInflater();
         return ltInflater.inflate(pathElementView, null, false);
     }
 
-    private void setPathOnClickAction(final View view, final String path){
-        view.setOnClickListener(new View.OnClickListener() {
+    private void clear() {
+        parent.removeAllViews();
+    }
+
+    private void scrollToEnd(final HorizontalScrollView view) {
+        view.post(new Runnable() {
             @Override
-            public void onClick(View view) {
-                if(handler!=null)
-                    handler.execute(path);
+            public void run() {
+                view.fullScroll(ScrollView.FOCUS_RIGHT);
             }
         });
     }
 
-    public static abstract class SelectPathHandler{
-        public abstract void execute(String fullpath);
+    private void setPathOnClickAction(@NonNull final View view, @NonNull final String path) {
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (handler != null)
+                    handler.execute(path);
+            }
+        });
     }
 }
--- AndroidClient/app/src/main/java/com/example/ya/filelocalshare/Path.java
+++ AndroidClient/app/src/main/java/com/example/ya/filelocalshare/Path.java
@@ -7,10 +7,10 @@ public class Path {
     public Path(String path){
         String[] parts = path.split("[/]");
         pathElements = new String[parts.length-3];
-        pathElements[0]=parts[1]+"/"+parts[2]+"/"+parts[3];
+        pathElements[0]=parts[1]+"/"+parts[2]+"/"+parts[3]+"/";
         Log.d("PATH",  parts[0]+"");
         for(int i =4; i<parts.length; i++)
-            pathElements[i-3]=parts[i];
+            pathElements[i-3]=parts[i]+"/";
 
     }
     public String[] getPathElements(){
