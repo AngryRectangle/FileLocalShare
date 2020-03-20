b63ec8f9f5a2b7723ad7c5c32357456e04d50d3e
AngryRectangle <yashmel2000@gmail.com>
2020-03-21 01:17:58 +0400

    AndroidBrowser refactoring

--- AndroidClient/app/src/main/java/com/example/ya/filelocalshare/AndroidBrowser.java
+++ AndroidClient/app/src/main/java/com/example/ya/filelocalshare/AndroidBrowser.java
@@ -2,7 +2,6 @@ package com.example.ya.filelocalshare;
 
 import android.app.Activity;
 import android.os.AsyncTask;
-import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.LinearLayout;
@@ -10,89 +9,191 @@ import android.widget.TableLayout;
 import android.widget.TableRow;
 
 import androidx.activity.OnBackPressedCallback;
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 import com.example.ya.filelocalshare.sort.FileSorter;
 
 import java.io.File;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 
 public class AndroidBrowser implements FileBrowser {
-    Activity activity;
-    TableLayout fileView;
-    FileViewer fileViewer;
-    FilePathShower pathViewer;
-    Map<String,Integer> icons;
-    HashSet<String> thumbnailExtensions;
-    FileViewer.FileViewOptions options;
-    AsyncSearch asyncSearch;
+    private final Activity activity;
+    private final TableLayout fileView;
+    private final FileViewer fileViewer;
+    private final FilePathShower pathViewer;
+    private final Map<String, Integer> icons;
+    private final HashSet<String> thumbnailExtensions;
+    private final FileViewer.FileViewOptions options;
+    private AsyncSearch asyncSearch;
     FileExplorer explorer;
 
-    public AndroidBrowser(final Activity activity, final TableLayout fileView, LinearLayout pathLayout, final FileExplorer explorer, @Nullable Map<String, Integer> icons, @Nullable HashSet<String> thumbnailExtensions, @Nullable FileViewer.FileViewOptions options) {
+    public AndroidBrowser(
+            @NonNull final Activity activity,
+            @NonNull final TableLayout fileView,
+            @NonNull LinearLayout pathLayout, final
+            @NonNull FileExplorer explorer,
+            @Nullable Map<String, Integer> icons,
+            @Nullable HashSet<String> thumbnailExtensions,
+            @Nullable FileViewer.FileViewOptions options
+    ) {
         this.activity = activity;
         this.fileView = fileView;
-        if(icons==null) {
+        fileViewer = new FileViewer(icons, thumbnailExtensions);
+        pathViewer = new FilePathShower(activity, R.layout.path_view, pathLayout);
+        this.explorer = explorer;
+        if (icons == null) {
             icons = new HashMap<>();
             icons.put("unknown", R.drawable.ic_file_icon_txt);
         }
         this.icons = icons;
-        if(thumbnailExtensions==null)
+        if (thumbnailExtensions == null)
             thumbnailExtensions = new HashSet<>();
         this.thumbnailExtensions = thumbnailExtensions;
-        fileViewer = new FileViewer(icons, thumbnailExtensions);
-        pathViewer = new FilePathShower(activity, R.layout.path_view, pathLayout);
-        if(options==null)
+        if (options == null)
             options = new FileViewer.FileViewOptions(3);
         this.options = options;
-        this.explorer = explorer;
-        OnBackPressedCallback callback = new OnBackPressedCallback(true) {
-            @Override
-            public void handleOnBackPressed() {
-                if(asyncSearch!=null&&(asyncSearch.getStatus().equals(AsyncTask.Status.RUNNING)||asyncSearch.getStatus().equals(AsyncTask.Status.FINISHED))) {
-                    cancelSearching();
-                }else {
-                    String[] path = explorer.currentPath.split("[/]");
-                    if(path.length>3) {
-                        String fullPath = "";
-                        for (int i = 0; i < path.length-1; i++)
-                            fullPath+=path[i]+"/";
-                        explorer.openDirectory(fullPath);
-                    }
+
+        setHandlers();
+    }
+
+    public void displayFiles(@NonNull File[] files) {
+        fileViewer.viewFiles(files, options);
+    }
+
+    public void displayFile(@NonNull File file) {
+        fileViewer.viewFile(file, options);
+    }
+
+    public void displayPath(@NonNull String path) {
+        pathViewer.showPath(path, explorer);
+    }
+
+    public void clearFileView() {
+        fileViewer.clear();
+    }
+
+    public void startSearching(@NonNull File dir, @NonNull String target) {
+        clearFileView();
+        if (asyncSearch != null && asyncSearch.getStatus().equals(AsyncTask.Status.RUNNING))
+            asyncSearch.cancel(false);
+        asyncSearch = new AsyncSearch();
+        asyncSearch.dir = dir;
+        asyncSearch.execute(target);
+    }
+
+    public void cancelSearching() {
+        asyncSearch.cancel(false);
+        asyncSearch = null;
+        clearFileView();
+        displayFiles(explorer.getFiles());
+        ((EditText) activity.findViewById(R.id.searchText)).setText(null);
+        activity.findViewById(R.id.searchText).clearFocus();
+        pathViewer.showPath(explorer.currentPath, explorer);
+    }
+
+    private class AsyncSearch extends AsyncTask<String, File, Void> {
+        File dir;
+
+        @Override
+        protected Void doInBackground(String... targetString) {
+            ArrayList<File> dirs = new ArrayList<>();
+            dirs.add(dir);
+            File[] files;
+            for (int i = 0; i < dirs.size() && !isCancelled(); i++) {
+                files = dirs.get(i).listFiles();
+                for (int j = 0; j < files.length; j++) {
+                    if (files[j].getName().contains(targetString[0]))
+                        publishProgress(files[j]);
+                    if (files[j].isDirectory())
+                        dirs.add(files[j]);
                 }
             }
-        };
-        ((MainActivity)activity).getOnBackPressedDispatcher().addCallback((MainActivity)activity, callback);
-        FileExplorer.ExplorerCallback viewFilesCallback = new FileExplorer.ExplorerCallback(){
+            return null;
+        }
+
+        @Override
+        protected void onProgressUpdate(File... file) {
+            if (!isCancelled()) displayFile(file[0]);
+        }
+    }
+
+    private void sortViewedFiles(@NonNull FileSorter.SortType type) {
+        ArrayList<File> sortedFiles = FileSorter.sort(fileViewer.getViewedFiles(), type);
+        File[] fileToView = sortedFiles.toArray(new File[0]);
+        fileViewer.clear();
+        fileViewer.viewFiles(fileToView, options);
+    }
+
+    private boolean isSearchUsed(){
+        return asyncSearch != null && (
+                asyncSearch.getStatus().equals(AsyncTask.Status.RUNNING) ||
+                        asyncSearch.getStatus().equals(AsyncTask.Status.FINISHED));
+    }
+
+    private void viewPreviousPath(){
+        String[] path = explorer.currentPath.split("[/]");
+        if (path.length > 3) {
+            String fullPath = "";
+            for (int i = 0; i < path.length - 1; i++)
+                fullPath += path[i] + "/";
+            explorer.openDirectory(fullPath);
+        }
+    }
+
+    private void setHandlers(){
+        setBackButtonHandler();
+        setFileViewHandler();
+        setStartSearchingCallback();
+        setSelectPathHandler();
+        setSortChangeHandler();
+        setClearViewHandler();
+    }
+    private void setBackButtonHandler() {
+        MainActivity main = (MainActivity) activity;
+        main.getOnBackPressedDispatcher().addCallback(
+                main,
+                new OnBackPressedCallback(true) {
+                    @Override
+                    public void handleOnBackPressed() {
+                        if (isSearchUsed())
+                            cancelSearching();
+                        else
+                            viewPreviousPath();
+                    }
+                });
+    }
+    private void setFileViewHandler(){
+        explorer.addOpenDirListener(new FileExplorer.ExplorerCallback() {
             @Override
-            public void execute(String path){
+            public void execute(String path) {
                 displayFiles(explorer.getFiles());
                 displayPath(path);
             }
-        };
-        explorer.addOpenDirListener(viewFilesCallback);
-        FileExplorer.ExplorerCallback startSearchingCallback = new FileExplorer.ExplorerCallback(){
+        });
+    }
+    private void setStartSearchingCallback() {
+        explorer.addStartSearchingListener(new FileExplorer.ExplorerCallback() {
             @Override
-            public void execute(String target){
+            public void execute(String target) {
                 startSearching(new File(explorer.currentPath), target);
                 pathViewer.showPath(explorer.currentPath, explorer);
                 pathViewer.addElement("Search", null, null, false);
             }
-        };
-        explorer.addStartSearchingListener(startSearchingCallback);
-
-        FilePathShower.SelectPathHandler selectPathHandler = new FilePathShower.SelectPathHandler() {
+        });
+    }
+    private void setSelectPathHandler(){
+        pathViewer.handler = new FilePathShower.SelectPathHandler() {
             @Override
             public void execute(String fullpath) {
                 explorer.openDirectory(fullpath);
             }
         };
-
-        pathViewer.handler = selectPathHandler;
+    }
+    private void setSortChangeHandler(){
         explorer.addSortChangeListener(new FileExplorer.SortChangeCallback() {
             @Override
             public void execute(FileSorter.SortType type) {
@@ -118,74 +219,13 @@ public class AndroidBrowser implements FileBrowser {
                 row.addView(view);
             }
         };
+    }
+    private void setClearViewHandler(){
         fileViewer.clearViewHandler = new FileViewer.ClearViewHandler() {
             @Override
             public void execute() {
                 fileView.removeAllViews();
             }
         };
-    }
-
-    public void displayFiles(File[] files){
-        fileViewer.viewFiles(files, options);
-    }
-    public void displayFile(File file){
-        fileViewer.viewFile(file, options);
-    }
-    public void displayPath(String path){
-        pathViewer.showPath(path, explorer);
-    }
-    public void clearFileView(){
-        fileViewer.clear();
-    }
-    public void startSearching(File dir, String target){
-        clearFileView();
-        if(asyncSearch!=null&&asyncSearch.getStatus().equals(AsyncTask.Status.RUNNING))
-            asyncSearch.cancel(false);
-        asyncSearch = new AsyncSearch();
-        asyncSearch.dir = dir;
-        asyncSearch.execute(target);
-    }
-    public void cancelSearching(){
-        asyncSearch.cancel(false);
-        asyncSearch=null;
-        clearFileView();
-        displayFiles(explorer.getFiles());
-        ((EditText)activity.findViewById(R.id.searchText)).setText(null);
-        activity.findViewById(R.id.searchText).clearFocus();
-        pathViewer.showPath(explorer.currentPath, explorer);
-    }
-
-    private void sortViewedFiles(FileSorter.SortType type){
-        ArrayList<File> sortedFiles = FileSorter.sort(fileViewer.getViewedFiles(), type);
-        File[] fileToView = sortedFiles.toArray(new File[0]);
-        fileViewer.clear();
-        fileViewer.viewFiles(fileToView, options);
-    }
-
-    private class AsyncSearch extends AsyncTask<String, File, Void> {
-        File dir;
-        FileExplorer explorer;
-        @Override
-        protected Void doInBackground(String... targetString) {
-            ArrayList<File> dirs = new ArrayList<>();
-            dirs.add(dir);
-            File[] files;
-            for(int i =0; i<dirs.size()&&!isCancelled(); i++){
-                files = dirs.get(i).listFiles();
-                for(int j = 0; j<files.length; j++){
-                    if(files[j].getName().contains(targetString[0]))
-                        publishProgress(files[j]);
-                    if(files[j].isDirectory())
-                        dirs.add(files[j]);
-                }
-            }
-            return null;
-        }
-
-        @Override
-        protected void onProgressUpdate(File... file) {
-            if(!isCancelled())displayFile(file[0]);
-        }
     }
 }
